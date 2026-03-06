20124766e32d6c4653ffb5de4d3c420091546993
dilora-gif <sharipovadilora@gmail.com>
2026-03-06 21:02:34 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// =====================================================
+// TELEGRAM BOT SOZLAMALARI
+// Bu faylni hech kimga yuborma va internetga chiqarma!
+// =====================================================
+const TELEGRAM_BOT_TOKEN = '7977837907:AAGZGSw0Bc4L2dlze8EorPlJObl2DeMawFQ';
+const TELEGRAM_CHAT_ID = '6857296724';
